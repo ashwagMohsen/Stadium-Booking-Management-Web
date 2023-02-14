08203d016752c4f3c10cadb8dada0dd3a3a0c086
ashwagMohsen <79316100+ashwagMohsen@users.noreply.github.com>
2023-02-14 04:21:41 +0300

    Add files via upload

---
+++ StadiumBookingProject/StadiumBookingProject/Models/Accounts2.cs
@@ -0,0 +1,34 @@
+﻿
+namespace StadiumBookingProject.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public partial class Accounts2
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Accounts2()
+        {
+            this.Reservations = new HashSet<Reservations>();
+        }
+
+        public int Id { get; set; }
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Required]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "كلمة المرور و التأكيد غير متطابقتان!!!")]
+        [Required]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        public string WonerType { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Reservations> Reservations { get; set; }
+    }
+}
---
+++ StadiumBookingProject/StadiumBookingProject/Models/Reservations2.cs
@@ -0,0 +1,70 @@
+﻿
+namespace StadiumBookingProject.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Reservations2
+    {
+        public int id { get; set; }
+        [Required]
+        [Display(Name = "التاريخ المختار")]
+        public string date { get; set; }
+        [Required]
+        [Display(Name = "الاسم الكامل")]
+
+        public string full_name { get; set; }
+        [Required]
+        [Display(Name = "الساعات المختارة")]
+
+        public Nullable<int> hours { get; set; }
+        [Required]
+        [Display(Name = "العربون")]
+
+        public string earnest { get; set; }
+        [Required]
+        [Display(Name = "المتبقي")]
+
+        public string residual { get; set; }
+        [Required]
+        [Display(Name = "السعر كامل")]
+
+        public string price { get; set; }
+        [Required]
+        [Display(Name = "الوقت المختار")]
+
+        public int time { get; set; }
+        [Required]
+        [Display(Name = "الجوال")]
+
+        public string mobile { get; set; }
+        [Required]
+        [Display(Name = "PayPal رقم")]
+
+        public int Pay_Pal_Number { get; set; }
+        [Required]
+        [Display(Name = "VIP رقم")]
+
+        public int VIP { get; set; }
+        [Required]
+        [Display(Name = "رقم الملعب")]
+
+        public Nullable<int> Stadium_Id { get; set; }
+        [Required]
+
+        public Nullable<int> Acc_Id { get; set; }
+        [Required]
+        [Display(Name = "الخدمات")]
+
+        public Nullable<int> Serv_Id { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "حساب البريد الإلكتروني")]
+
+        public string Email { get; set; }
+        public virtual Accounts Accounts { get; set; }
+        public virtual Stadiums Stadiums { get; set; }
+        public virtual Services Services { get; set; }
+    }
+}
