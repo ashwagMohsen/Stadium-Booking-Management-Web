4e59b4e4c972bf1e7c133847206e07aa380a9267
ashwagMohsen <79316100+ashwagMohsen@users.noreply.github.com>
2023-02-14 04:18:14 +0300

    Add files via upload

---
+++ StadiumBookingProject/StadiumBookingProject/Controllers/ReservationsController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StadiumBookingProject.Models;
+
+namespace StadiumBookingProject.Controllers
+{
+    public class ReservationsController : Controller
+    {
+        private BookingEntities db = new BookingEntities();
+
+        // GET: Reservations
+        //[Authorize(Roles = "Admin")]
+        //[Authorize]
+        public ActionResult Index()
+        {
+            var reservations = db.Reservations.Include(r => r.Accounts).Include(r => r.Stadiums).Include(r => r.Services);
+            return View(reservations.ToList());
+        }
+        [HttpPost]
+        public ActionResult Index(string name)
+        {
+            var reservations = db.Reservations.Include(r => r.Accounts).Include(r => r.Stadiums).Include(r => r.Services);
+            return View(reservations.Where(a => a.full_name == name).ToList());
+        }
+        // GET: Reservations/Details/5
+        //[Authorize(Roles = "Admin")]
+        //[Authorize]
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reservations reservations = db.Reservations.Find(id);
+            if (reservations == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reservations);
+        }
+
+        // GET: Reservations/Create
+        //[Authorize(Users = "Client")]
+        [AllowAnonymous]
+        public ActionResult Create(int S_id)
+        {
+            var y = db.Stadiums.Where(a => a.Id == S_id).ToList();
+            if (y.Count != 0)
+            {
+                ViewBag.hour = y[0].Hour;
+                ViewBag.price1 = y[0].Price;
+                ViewBag.Stadium_Id = y[0].Name;
+            }
+           
+        //ViewBag.Acc_Id = new SelectList(db.Accounts, "Id", "Email");
+            //ViewBag.Stadium_Id = new SelectList(db.Stadiums.Where(s => s.Id==S_id), "Id", "Name");
+            ViewBag.Serv_Id = new SelectList(db.Services, "Serv_Id", "Serv_Name");
+            return View();
+        }
+        [AllowAnonymous]
+        // POST: Reservations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(string date, string full_name, string earnest, string residual, int time, string mobile, string Stadium_Id, int Serv_Id, string Email,int hours, string price)
+        {
+            Reservations reservations2 = new Reservations();
+            var E_Id = db.Accounts.Where(a => a.Email ==Email).ToList();
+            var S_Id = db.Stadiums.Where(a => a.Name == Stadium_Id).ToList();
+            if (ModelState.IsValid)
+            {
+                reservations2.Acc_Id = E_Id[0].Id; reservations2.date = date; reservations2.full_name = full_name; reservations2.earnest = earnest;
+                reservations2.residual = residual; reservations2.time = time; reservations2.mobile = mobile; reservations2.Stadium_Id = S_Id[0].Id; reservations2.Serv_Id = Serv_Id;
+                reservations2.price = price;reservations2.hours = hours;
+                var list1 = db.Reservations.Where(a => a.Stadium_Id == reservations2.Stadium_Id && a.date == date && a.time == time).ToList();
+
+                if (list1.Count != 0)
+                {
+                    ViewBag.message = "!!عذرا هذا التاريخ محجوز ,اختر تاريخ اخر";
+                    var y2 = db.Stadiums.Where(a => a.Id == reservations2.Stadium_Id).ToList();
+                    if (y2.Count != 0)
+                    {
+                        ViewBag.hour = y2[0].Hour;
+                        ViewBag.price = y2[0].Price;
+                        ViewBag.Stadium_Id = y2[0].Name;
+                    }
+
+                    ViewBag.Acc_Id = new SelectList(db.Accounts, "Id", "Email");
+                    ViewBag.Serv_Id = new SelectList(db.Services, "Serv_Id", "Serv_Name");
+                    return View();
+                }
+                else
+                {
+                    db.Reservations.Add(reservations2);
+                    db.SaveChanges();
+                    var x = db.Reservations.ToList();
+                    int id = x[(x.Count) - 1].id;
+                    return RedirectToAction("../StadiumBooking/Invoice", new { id1 = id });
+                }
+       
+            }
+            var y = db.Stadiums.Where(a => a.Id == S_Id[0].Id).ToList();
+            if (y.Count != 0)
+            {
+                ViewBag.hour = y[0].Hour;
+                ViewBag.price = y[0].Price;
+                ViewBag.Stadium_Id = y[0].Name;
+
+            }
+            ViewBag.Acc_Id = new SelectList(db.Accounts, "Id", "Email");
+            ViewBag.Serv_Id = new SelectList(db.Services, "Serv_Id", "Serv_Name");
+            return View();
+        }
+
+
+
+        // GET: Reservations/Delete/5
+        //[Authorize(Roles = "Admin")]
+        //[Authorize]
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reservations reservations = db.Reservations.Find(id);
+            if (reservations == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reservations);
+        }
+        //[Authorize]
+
+        // POST: Reservations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Reservations reservations = db.Reservations.Find(id);
+            db.Reservations.Remove(reservations);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //public JsonResult getLecture1(string S_name)
+        //{
+
+        //    IQueryable<Services> dbList = db.Services.Where(a => a.Serv_Name == S_name);
+
+        //    return Json(dbList.ToList(), JsonRequestBehavior.AllowGet);
+        //}
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ StadiumBookingProject/StadiumBookingProject/Controllers/StadiumBookingController.cs
@@ -0,0 +1,148 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using StadiumBookingProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace StadiumBookingProject.Controllers
+{
+    [AllowAnonymous]
+    public class StadiumBookingController : Controller
+    {
+        static bool connect=false ;
+        BookingEntities db = new BookingEntities();
+        // GET: StadiumBooking
+
+        public ActionResult Index()
+        {
+        
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(int id1)
+        {
+            if (connect == true)
+            {
+                return RedirectToAction("Create", "Reservations", new { S_id= id1 });
+
+            }
+            else if (connect == false)
+            {
+                return RedirectToAction("Login");
+
+            }
+            return View();
+        }
+        //[Authorize(Users =TempData["woner"])]
+        //[Authorize(Users = "Client")]
+        public ActionResult Invoice(int id1)
+        {
+            var list1 = db.Reservations.Where(a => a.id == id1);
+            return View(list1.ToList());
+        }
+      
+
+        //------------------------------Register--------------------------
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(string Email,string Password)
+        {
+            Accounts accounts1 = new Accounts();
+            // await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+            if (ModelState.IsValid)
+            {
+                accounts1.Email = Email; accounts1.Password = Password; accounts1.WonerType = "عميل";
+                db.Accounts.Add(accounts1);
+                db.SaveChanges();
+                //userManager.AddToUser("Client");
+                //TempData["woner"] = "Client";
+                connect = true;
+                return RedirectToAction("Index");
+            }
+            return View(accounts1);
+        }
+        //------------------------------LogIn--------------------------
+        public ActionResult Login()
+        {
+            //ViewBag.WonerType = new SelectList(db.Stadiums, "Id", "Name");
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string Email, string Password, string WonerType)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var list1 = db.Accounts.ToList();
+
+                    foreach (var i in list1)
+                    {
+                        if (i.Email == Email && i.Password == Password && i.WonerType== WonerType)
+                        {
+                            if (WonerType == "مدير")
+                            {
+                                //i.CreateIdentityAsync()
+                                //TempData["woner"] = "Admin";
+                                connect = true;
+                                return RedirectToAction("Index","Reservations");
+                            }
+                            else
+                            {
+                                connect = true;
+                                //TempData["woner"] = "Client";
+                                return RedirectToAction("Index");
+                            }
+
+                        }
+                        else
+                        {
+                            ViewBag.message="You must sign up at first !!!";
+                        }
+                    }
+                }
+                catch
+                {
+
+                }
+              
+                
+            }
+            return View();
+            //var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            //switch (result)
+            //{
+            //    case SignInStatus.Success:
+            //        return RedirectToLocal(returnUrl);
+            //    case SignInStatus.LockedOut:
+            //        return View("Lockout");
+            //    case SignInStatus.RequiresVerification:
+            //        return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+            //    case SignInStatus.Failure:
+            //    default:
+            //        ModelState.AddModelError("", "Invalid login attempt.");
+            //        return View(model);
+            //}
+        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            connect = false;
+            return RedirectToAction("Index", "StadiumBooking");
+        }
+    }
+}
